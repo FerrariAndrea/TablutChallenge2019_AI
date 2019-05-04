b52357349bd401bef038be84dfc852eb2284fee7
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-05-04 11:57:14 +0200

    TablutState V1

--- TablutAI/src/search/TablutState.java
+++ TablutAI/src/search/TablutState.java
@@ -5,97 +5,23 @@ import aima.core.agent.Action;
 import aima.core.search.framework.evalfunc.HeuristicFunction;
 import aima.core.search.framework.problem.GoalTest;
 import aima.core.search.framework.problem.StepCostFunction;
+import it.unibo.ai.didattica.competition.tablut.domain.StateTablut;
 
 
 
+public class TablutState implements GoalTest,StepCostFunction,HeuristicFunction {
 
-public class TablutState 
-	implements 	GoalTest,
-					StepCostFunction,
-					HeuristicFunction {
-
-	// Actions
-	static public final String mc = "MC";
-	static public final String mm = "MM";
-	static public final String cc = "CC";
-	static public final String m = "M";
-	static public final String c = "C";
-	
-	/*
-	public static Action MC = new DynamicAction("MC");
-	public static Action MM = new DynamicAction("MM");
-	public static Action CC = new DynamicAction("CC");
-	public static Action M = new DynamicAction("M");
-	public static Action C = new DynamicAction("C");
-	*/
-	
 	
 	
 	
 /*****************************************************************************
  * Description of the state of the problem
 /*****************************************************************************/
-	/*
-	 * Please note that I have a little far extended representation of the state.
-	 * 
-	 * Through this representation it is possible to map also problems with
-	 * a higher number of Missionars and Cannibals, as well as to experiment
-	 * different configurations of the initial state (i.e. not all the people on
-	 * the same shore in the beginning) 
-	 */
-	
-	// total number of Missionars at the beginning of the game
-	private final int totMissionars;
-	
-	// number of Missionars at the starting shore
-	private int missionars;
-	
-   // total number of Cannibals at the beginning of the game
-	private final int totCannibals;
-	
-	// number of Cannibals at the starting shore
-	private int cannibals;
-	
-	/*
-	 * true means that it is on this shore, false means that is on the other shore
-	 */
-	private boolean posBoat;
- 
+	private  StateTablut state;
  
 /*****************************************************************************
  * Methods for accessing the state
 /*****************************************************************************/
-	/**
-	 * @return Returns the cannibals at the starting shore.
-	 */
-	public int getCannibals() {
-		return cannibals;
-	}
-	/**
-	 * @return Returns the missionars at the starting shore.
-	 */
-	public int getMissionars() {
-		return missionars;
-	}
-	/**
-	 * @return Returns true if the boat is at the starting shore.
-	 */
-	public boolean isPosBoat() {
-		return posBoat;
-	}
-	/**
-	 * @return Returns the total number ofCannibals.
-	 */
-	public int getTotCannibals() {
-		return totCannibals;
-	}
-	/**
-	 * @return Returns the total number of Missionars.
-	 */
-	public int getTotMissionars() {
-		return totMissionars;
-	}
-	
 	
 	
 	
@@ -111,7 +37,7 @@ public class TablutState
 	 * It initializes the problem in the classic configuration (3,3,1).
 	 */
 	public TablutState() {
-		this(3, 3, 3, 3, true);
+		this.state= new StateTablut();
 	}
 
 	/*
@@ -120,22 +46,11 @@ public class TablutState
 	 * Used for generating new states, as well as for creating different 
 	 * instances of the problem (with more people, and maybe on different shores)
 	 */
-	public TablutState( int totMissionars,
-												int totCannibals,
-												int missionarsOnThisShore,
-												int cannibalsOnThisShore,
-												boolean posBoat) {
-		this.totMissionars = totMissionars;
-		this.missionars = missionarsOnThisShore;
-		this.totCannibals = totCannibals;
-		this.cannibals = cannibalsOnThisShore;
-		this.posBoat = posBoat;
+	public TablutState(StateTablut state) {
+		this.state=state;
 	}
-	
-	
-	
-	
-	
+
+
 	
 	
 /*****************************************************************************
@@ -144,10 +59,7 @@ public class TablutState
 	public boolean isGoalState(Object state) {
 		if (state instanceof TablutState) {
 			TablutState mcState = (TablutState) state;
-		   return (		(mcState.missionars == 0) && 
-		    				(mcState.cannibals == 0) &&
-							(mcState.posBoat == false)
-			);
+		   return (	this.state.getTurn().compareTo(Turn.win));
 		}
 		else
 			return false;
