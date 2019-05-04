aa9e31e01f2c271179a50a51669f8b2c1b2908d0
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-05-04 18:37:09 +0200

    TablutState V3, Definite le mosse possibili per i neri

--- TablutAI/src/search/BlackTablutState.java
+++ TablutAI/src/search/BlackTablutState.java
@@ -5,7 +5,7 @@ package search;
 import it.unibo.ai.didattica.competition.tablut.domain.StateTablut;
 
 
-public class BlackTablutState  extends AITablutState {
+public class BlackTablutState extends AITablutState {
 
 	public BlackTablutState(StateTablut state) {
 		super(state, false);
--- TablutAI/src/search/TablutResultFunction.java
+++ TablutAI/src/search/TablutResultFunction.java
@@ -9,98 +9,13 @@ public class TablutResultFunction implements ResultFunction {
 	
 	public Object result(Object o, Action a) {
 		
-		BlackTablutState mcState = (BlackTablutState) o;
-		if (TablutActionsFunction.MC.equals(a))
-			return moveMC(mcState);
-		if (TablutActionsFunction.MM.equals(a))
-			return moveMM(mcState);
-		if (TablutActionsFunction.CC.equals(a))
-			return moveCC(mcState);
-		if (TablutActionsFunction.M.equals(a))
-			return moveM(mcState);
-		if (TablutActionsFunction.C.equals(a))
-			return moveC(mcState);
+		AITablutState mcState = (AITablutState) o;
 		
-		//noOp
+		mcState.getState()
+		
 		return mcState;
 	}
 	
-
-
-	private BlackTablutState moveMC(BlackTablutState mcState) {
-		if (mcState.isPosBoat())
- 			return new BlackTablutState(mcState.getTotMissionars(),
- 															mcState.getTotCannibals(),
-															mcState.getMissionars() - 1,
-															mcState.getCannibals() - 1,
-															! mcState.isPosBoat());
- 		else
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars() + 1,
-															mcState.getCannibals() + 1,
-															! mcState.isPosBoat());
- 	}
-	
- 	private BlackTablutState moveMM(BlackTablutState mcState) {
- 		if (mcState.isPosBoat())
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars() - 2,
-															mcState.getCannibals(),
-															! mcState.isPosBoat());
- 		else
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars() + 2,
-															mcState.getCannibals(),
-															! mcState.isPosBoat());
- 	}
-
- 	private BlackTablutState moveCC(BlackTablutState mcState) {
- 		if (mcState.isPosBoat())
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars(),
-															mcState.getCannibals() - 2,
-															! mcState.isPosBoat());
- 		else
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars(),
-															mcState.getCannibals() + 2,
-															! mcState.isPosBoat());
- 	}
-	
- 	private BlackTablutState moveM(BlackTablutState mcState) {
- 		if (mcState.isPosBoat())
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars() - 1,
-															mcState.getCannibals(),
-															! mcState.isPosBoat());
- 		else
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars() + 1,
-															mcState.getCannibals(),
-															! mcState.isPosBoat());
- 	}
- 	
- 	private BlackTablutState moveC(BlackTablutState mcState) {
- 		if (mcState.isPosBoat())
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars(),
-															mcState.getCannibals() - 1,
-															! mcState.isPosBoat());
- 		else
- 			return new BlackTablutState(mcState.getTotMissionars(),
-															mcState.getTotCannibals(),
-															mcState.getMissionars(),
-															mcState.getCannibals() + 1,
-															! mcState.isPosBoat());
- 	}
 
 	
 	
